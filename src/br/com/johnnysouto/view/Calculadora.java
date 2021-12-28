ed5aa385e8056c5494a269bdb60ca24a583136a0
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-12-28 00:21:09 -0300

    Update Calculadora.java
    criando as funções da calculadora

--- src/br/com/johnnysouto/view/Calculadora.java
+++ src/br/com/johnnysouto/view/Calculadora.java
@@ -5,5 +5,21 @@ public class Calculadora {
 	public static void main(String[] args) {
 
 	}
+	
+	public static int somar(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtrair(int a, int b) {
+		return a - b;
+	}
+	
+	public static int dividir(int a, int b) {
+		return a / b;
+	}
+	
+	public static int multiplicar(int a, int b) {
+		return a * b;
+	}
 
 }
