51f5624b37f9e51261c8b8a51a3d3fb4027cdcec
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-12-28 00:22:23 -0300

    Update Calculadora.java
    importando o scanner

--- src/br/com/johnnysouto/view/Calculadora.java
+++ src/br/com/johnnysouto/view/Calculadora.java
@@ -1,8 +1,12 @@
 package br.com.johnnysouto.view;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
 
 	}
 	
